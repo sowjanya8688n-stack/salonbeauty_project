e10ef51de0b0e8ab20e23dc65dc10d8b14577a94
Your Name <your-email@example.com>
2026-08-31 22:11:55 +0530

    Connect frontend to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000"
-  baseURL: "https://salonbeauty-backend.onrender.com"
+  // baseURL: "https://salonbeauty-backend.onrender.com"
+  baseURL: "https://salonbeauty-backend.onrender.com",
 });
 
 export default api;
