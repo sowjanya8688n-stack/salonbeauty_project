8f15b5084e797abd19db37f8fbbef61bfeacf3ae
Your Name <your-email@example.com>
2026-08-31 22:36:56 +0530

    Fix admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 // 
 import { useLocation } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AllRoutes from "./AllRoutes";
 
 function App() {
+  const location = useLocation();
+
+  // Check whether current page is an admin page
+  const isAdminPage = location.pathname.startsWith("/admin");
+
   return (
     <>
-      <Navbar />
+      {/* Show Navbar only for user pages */}
+      {!isAdminPage && <Navbar />}
 
+      {/* All user and admin routes */}
       <AllRoutes />
 
-      <Footer />
+      {/* Show Footer only for user pages */}
+      {!isAdminPage && <Footer />}
     </>
   );
 }
